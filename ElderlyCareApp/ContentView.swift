bc1d3a361c7a4e68729fc877576bc769e62c5f67
Mohan <kummariguntam5769@gmail.com>
2024-09-14 23:05:58 -0700

    Initial Commit

---
+++ ElderlyCareApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElderlyCareApp
+//
+//  Created by MOHAN KUMMARIGUNTA on 9/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElderlyCareApp/ElderlyCareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElderlyCareAppApp.swift
+//  ElderlyCareApp
+//
+//  Created by MOHAN KUMMARIGUNTA on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ElderlyCareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
